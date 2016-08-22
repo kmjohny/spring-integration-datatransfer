0e17180a47b094036bfa5fd92445b37277ed12e8
Johny K Mathew <kmjohny@gmail.com>
2016-08-22 10:14:31 -0400

    Setup Spring Integration starter project

---
+++ src/main/java/org/jkm/springintegration/datatransfer/SpringIntegrationDatatransferApplication.java
@@ -0,0 +1,12 @@
+package org.jkm.springintegration.datatransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationDatatransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationDatatransferApplication.class, args);
+	}
+}
---
+++ src/test/java/org/jkm/springintegration/datatransfer/SpringIntegrationDatatransferApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jkm.springintegration.datatransfer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringIntegrationDatatransferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
